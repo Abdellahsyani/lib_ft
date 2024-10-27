5be45ff65e7481d6c16d20261963a4484e4353a0
Abdellahsyani <syaniabdos@gmail.com>
2024-10-27 19:09:24 +0100

    modified split

--- ft_split.c
+++ ft_split.c
@@ -6,25 +6,26 @@
 /*   By: asyani <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 12:11:09 by asyani            #+#    #+#             */
-/*   Updated: 2024/10/27 16:13:41 by asyani           ###   ########.fr       */
+/*   Updated: 2024/10/27 19:09:06 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*count_words(const char *str, char set)
+int	count_words(const char *str, char set)
 {
 	int	i;
-	char	*word;
+	int	j;
 
 	i = 0;
+	j = 1;
 	while (str[i] != '\0')
 	{
 		if (str[i] != set)
-			word++;
+			j++;
 		i++;
 	}
-	return (word);
+	return (j);
 }
 
 char *ft_strdup(const char *s)
@@ -41,22 +42,36 @@ char *ft_strdup(const char *s)
 		dup[i] = s[i];
 		i++;
 	}
+	dup[i] = '\0';
 	return (dup);
 }
 
-char	**ft_split(char const *s, char c)
+char    **ft_split(char const *s, char c)
 {
-	int	i;
-	char	*word;
-	char	**dups;
+    int    i;
+    int    strings_s;
+    char    **dups;
 
-	while (s[i] != '\0')
-	{
-		word = count_words(s, c);
-		*dups = ft_strdup(word);
-		i++;
-	}
-	return (&dups);
+    i = 0;
+    int j = 0;
+    strings_s = count_words(s, c);
+    dups = malloc(sizeof(char **) * (strings_s) + 1);
+    if (!dups)
+        return (NULL);
+    while (i < strings_s)
+    {
+        while (s[j] != '\0')
+        {
+            if (s[j] != c)
+            {
+                *dups = ft_strdup(s);
+                dups++;
+            }
+           j++;
+        }
+        i++;
+    }
+    return (dups);
 }
 
 int main()
