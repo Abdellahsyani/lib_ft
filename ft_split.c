ed9fbc25b421244df6f43af22798fec99f48b52f
Abdellahsyani <syaniabdos@gmail.com>
2024-10-28 00:05:28 +0100

    modified solit

--- ft_split.c
+++ ft_split.c
@@ -12,73 +12,78 @@
 
 #include "libft.h"
 
-int	count_words(const char *str, char set)
+int count_words(const char *str, char set)
 {
-	int	i;
-	int	j;
+    int i = 0;
+    int count = 0;
+    int in_word = 0;
 
-	i = 0;
-	j = 1;
-	while (str[i] != '\0')
-	{
-		if (str[i] != set)
-			j++;
-		i++;
-	}
-	return (j);
-}
-
-char *ft_strdup(const char *s)
-{
-	int	i;
-	char	*dup;
-	int	size;
-
-	size = ft_strlen(s);
-	dup = malloc(sizeof(char) * size + 1);
-	i = 0;
-	while (s[i] != '\0')
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-
-char    **ft_split(char const *s, char c)
-{
-    int    i;
-    int    strings_s;
-    char    **dups;
-
-    i = 0;
-    int j = 0;
-    strings_s = count_words(s, c);
-    dups = malloc(sizeof(char **) * (strings_s) + 1);
-    if (!dups)
-        return (NULL);
-    while (i < strings_s)
+    while (str[i])
     {
-        while (s[j] != '\0')
+        if (str[i] != set && !in_word)
         {
-            if (s[j] != c)
-            {
-                *dups = ft_strdup(s);
-                dups++;
-            }
-           j++;
+            in_word = 1;
+            count++;
+        }
+        else if (str[i] == set)
+        {
+            in_word = 0;
         }
         i++;
     }
-    return (dups);
+    return count;
+}
+
+char *ft_strdup(const char *s, int len)
+{
+    char *dup = malloc(sizeof(char) * (len + 1));
+    if (!dup)
+        return NULL;
+
+    strncpy(dup, s, len);
+    dup[len] = '\0';
+    return dup;
+}
+
+char **ft_split(char const *s, char c)
+{
+    int i = 0;
+    int start = 0;
+    int j = 0;
+    int strings_s = count_words(s, c);
+    char **dups = malloc(sizeof(char *) * (strings_s + 1));
+    if (!dups)
+        return NULL;
+    while (i < strings_s)
+    {
+        while (s[j] == c)
+            j++;
+        start = j;
+        while (s[j] != c && s[j] != '\0')
+            j++;
+        if (j > start)
+        {
+            dups[i] = ft_strdup(s + start, j - start);
+            i++;
+        }
+    }
+    dups[i] = NULL;
+    return dups;
 }
 
 int main()
 {
-	char str[] = "hellocworld";
-	char c = 'c';
+    char str[] = "hello c world";
+    char c = 'c';
 
-	char **dup = ft_split(str, c);
-	printf("%s", dup);
+    char **dup = ft_split(str, c);
+
+    for (int i = 0; dup[i] != NULL; i++)
+    {
+        printf("%s\n", dup[i]);
+        free(dup[i]);
+    }
+    free(dup);
+    return 0;
 }
+
