6edee52f1d4baafd073df78ea471dbac084ef4d8
Abdellahsyani <syaniabdos@gmail.com>
2024-10-30 18:39:37 +0100

    add

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,15 +6,18 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 16:43:35 by asyani            #+#    #+#             */
-/*   Updated: 2024/10/22 17:46:54 by asyani           ###   ########.fr       */
+/*   Updated: 2024/10/30 18:39:21 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	size_t			i;
 	unsigned char	*dest;
 
+	i = 0;
 	dest = (unsigned char *)dst;
 	while (i < n)
 	{
