8b423519e65d55f46f59d4faf4f2b7c8ae8bbec7
Abdellahsyani <syaniabdos@gmail.com>
2024-11-08 11:51:04 +0100

    add all

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:29:42 by asyani            #+#    #+#             */
-/*   Updated: 2024/11/06 16:43:55 by asyani           ###   ########.fr       */
+/*   Updated: 2024/11/08 10:27:19 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (count > SIZE_MAX / size)
+		return (NULL);
 	alloc = malloc(count * size);
 	if (!alloc)
 		return (NULL);
