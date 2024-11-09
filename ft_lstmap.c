3d8c3705787a5dbdde291927e73d9d270dc50008
Abdellahsyani <syaniabdos@gmail.com>
2024-11-09 11:33:17 +0100

    last

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:09:13 by asyani            #+#    #+#             */
-/*   Updated: 2024/11/07 15:13:30 by asyani           ###   ########.fr       */
+/*   Updated: 2024/11/09 11:33:00 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,23 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
+	t_list	*new_node;
 
-	if (!lst)
-		return (NULL);
-	new_lst = malloc(sizeof(new_lst));
-	if (!new_lst)
+	if (!lst || !f)
 		return (NULL);
 	while (lst)
 	{
-		new_lst = f(lst->content);
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		if (!new_lst)
+			new_lst = new_node;
+		else
+			ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
-	del(new_lst->content);
 	return (new_lst);
 }
--- main.c
+++ main.c
@@ -1,5 +1,70 @@
 #include "libft.h"
+
+void del(void *content)
+{
+	free(content);
+}
+
 int main()
 {
-	printf("%ld", ft_strlen("hello"));
+	t_list *head = NULL;
+	t_list *current = NULL;
+	int n1 = 2;
+	int n2 = 3;
+	int n3 = 4;
+	int n4 = 5;
+	int n5 = 1;
+	int *a = malloc(1 * sizeof(int));
+	int *b = malloc(1 * sizeof(int));
+	int *c = malloc(1 * sizeof(int));
+	int *d = malloc(1 * sizeof(int));
+	int *e = malloc(1 * sizeof(int));
+	*a = n1;
+	*b = n2;
+	*c = n3;
+	*d = n4;
+	*e = n5;
+
+	//Create the node
+	t_list *node1 = ft_lstnew(a);
+	t_list *node2 = ft_lstnew(b);
+	t_list *node3 = ft_lstnew(c);
+	t_list *node4 = ft_lstnew(d);
+	t_list *node5 = ft_lstnew(e);
+
+	//add node to list
+	head = node1;
+	head->next = NULL;
+	ft_lstadd_back(&head, node2);
+	ft_lstadd_back(&head, node3);
+	ft_lstadd_back(&head, node4);
+	ft_lstadd_front(&head, node5);
+
+	//loop throug list
+	current = head;
+	while (current)
+	{
+		printf("%d-->", *(int *)current->content);
+		current = current->next;
+	}
+
+	//get the size of the list
+	current = head;
+	int i = ft_lstsize(current);
+	printf("\nthe size of list is: %d\n", i);
+
+	// get the last node
+	current = head;
+	t_list *last = ft_lstlast(current);
+	int *l = last->content;
+	printf("\nThe last node is: %d\n", l[0]);
+
+	// free the first node
+	ft_lstclear(&head, del);
+	current = head;
+	while(current)
+	{
+		printf("%d-->", *(int *)current->content);
+		current = current->next;
+	}
 }
